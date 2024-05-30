220144e603e4ce9d38b3456acf9010e65517fcb6
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-30 10:36:27 +0700

    menambahkan output, member

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -12,7 +12,14 @@ public:
     }
 
     void insert() {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
         
+            return;
+        }
     }
 };
 
