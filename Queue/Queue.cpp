1e2a0370c13b12e3467547828ebc212d16de2e6c
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-30 10:53:18 +0700

    menambahkan statement if, else

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -53,7 +53,11 @@ public:
             REAR = -1;
         }
         else {
-
+            // Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
         }
     }
 };
