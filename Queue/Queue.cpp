3aab1a9df70793f95ce7129240e4e431e9702972
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-30 10:36:42 +0700

    menambahkan statement if

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -17,7 +17,9 @@ public:
         cin >> num;
         cout << endl;
 
-        
+        // Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
             return;
         }
     }
