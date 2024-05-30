cfed5e6296eaae6a544ff626404bf8e591f2f71b
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-30 11:04:19 +0700

    menambahkan statement if

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -62,6 +62,11 @@ public:
     }
 
     void display() {
+        // Cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
 
     }
 };
